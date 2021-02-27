010d6c4fa1e1a711b8a2eab8179e16104afafacb
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-02-27 08:09:53 +0530

    My Java Solution

---
+++ Day<02>/MySolution.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+class GURU
+{
+    public static void main (String[] args)
+    {
+		    Scanner ob=new Scanner(System.in);
+		    int t = ob.nextInt();
+		    while (t-->0)
+		    {
+		        long n = ob.nextLong();
+		        Solution ab = new Solution();
+		        long k = ab.findNth(n);
+		        System.out.println(k);
+        }
+    }
+}
+
+class Solution
+{
+    long findNth(long N)
+    {
+        StringBuilder sss = new StringBuilder("");
+        while ( 0 < N )
+        {
+            sss.append(N % 9);
+            N /= 9;
+        }
+        return Long.valueOf(sss.reverse().toString());
+    }
+}
