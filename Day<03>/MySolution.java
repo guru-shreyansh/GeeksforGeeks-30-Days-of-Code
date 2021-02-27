4faff29fe3365bfbb124ef57121dba5e706c8cbf
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-02-27 09:13:37 +0530

    My Java Solution

--- Day<03>/MySolution.java
+++ Day<03>/MySolution.java
@@ -28,7 +28,7 @@ class Solution
     {
         Arrays.sort(array);
         long currNumber = 1L;               // Minimum positive output
-		for (int i = 0; i<array.length; i++)
+		for (int i=0; i<array.length; i++)
 		{
 			if (array[i] <= currNumber)
  				currNumber += array[i];
