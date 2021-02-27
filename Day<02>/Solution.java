401eeffa665c5166de0dfa46197e3b9113c4c999
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-02-27 08:15:10 +0530

    Java

--- Day<02>/Solution.java
+++ Day<02>/Solution.java
@@ -5,16 +5,16 @@ class GURU
 {
     public static void main (String[] args)
     {
-		    Scanner ob=new Scanner(System.in);
-		    int t = ob.nextInt();
-		    while (t-->0)
-		    {
-		        long n = ob.nextLong();
-		        Solution ab = new Solution();
-		        long k = ab.findNth(n);
-		        System.out.println(k);
-        }
-    }
+	  Scanner ob=new Scanner(System.in);
+	  int t = ob.nextInt();
+	  while (t-->0)
+	  {
+		long n = ob.nextLong();
+		Solution ab = new Solution();
+		long k = ab.findNth(n);
+		System.out.println(k);
+	  }
+     }
 }
 
 class Solution
