fbcb030d3dffa7746531ab84e39f94a0438c0c82
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-02-27 19:12:29 +0530

    Java

---
+++ Day<16>/Solution.java
@@ -0,0 +1,78 @@
+import java.util.*;
+import java.math.*;
+
+class Gfg
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.next());
+        while(t-- > 0)
+        {
+            int k = Integer.parseInt(sc.next());
+            String s = sc.next();
+            Solution T = new Solution();
+            System.out.println(T.reduced_String(k, s));
+            
+        }
+    }
+}
+// } Driver Code Ends
+
+class Solution
+{
+    public static String reduced_String(int k, String s)
+    {
+        if(k == 1){
+            String ans = "";
+            return ans;
+        }
+        Stack<Pair> st = new Stack<Pair>();
+        int l = s.length();
+        int ctr = 0;
+        for(int i=0;i<l;i++)
+        {
+            if(st.size() == 0)
+            {
+                st.push(new Pair(s.charAt(i),1));
+                continue;
+            }
+            if(st.peek().c == s.charAt(i))
+            {
+                Pair p = st.peek();
+                st.pop();
+                p.ctr += 1;
+                if(p.ctr == k)
+                {
+                    continue;
+                }
+                else
+                {
+                    st.push(p);
+                }
+            }
+            else{
+                 st.push(new Pair(s.charAt(i),1));
+            }
+        }
+        String ans = "";
+        while(st.size() > 0)
+        {
+            char c = st.peek().c;
+            int cnt = st.peek().ctr;
+            while(cnt-- > 0)
+                ans = c + ans;
+            st.pop();
+        }
+        return ans;
+    }
+}
+class Pair{
+    char c;
+    int ctr;
+    Pair(char c,int ctr)
+    {
+        this.c = c;
+        this.ctr = ctr;
+    }
+}
