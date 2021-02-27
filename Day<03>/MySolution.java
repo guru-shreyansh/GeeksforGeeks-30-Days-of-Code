d36d7f84ac31f53e18c0e29397e00434078e0e0a
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-02-27 09:12:43 +0530

    My Java Solution

---
+++ Day<03>/MySolution.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+class GURU
+{
+    public static void main(String args[]) throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0)
+        {
+            int n = sc.nextInt();
+            long array[] = new long[n];
+            for (int i=0; i<n; ++i)
+            {
+                array[i] = sc.nextLong();
+            }
+            Solution ob = new Solution();
+            System.out.println(ob.smallestpositive(array,n));
+            t--;
+        }
+    }
+}
+
+class Solution
+{
+    long smallestpositive(long[] array, int n)
+    {
+        Arrays.sort(array);
+        long currNumber = 1L;               // Minimum positive output
+		for (int i = 0; i<array.length; i++)
+		{
+			if (array[i] <= currNumber)
+ 				currNumber += array[i];
+ 			else 
+ 			    break;
+		}
+ 		return currNumber;
+    }
+}
