0a0d3176ed8f538963bed22ecc2b369fb8e9718b
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-02-27 16:35:14 +0530

    C++

---
+++ Day<07>/Solution.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    
+    public:
+    long long ValidPair(int a[], int n) 
+    { 
+    	sort(a, a+n); 
+    	long long ans = 0; 
+    	for (int i = 0; i < n; ++i) { 
+    		if (a[i] <= 0) 
+    			continue; 
+    		// search for first element >= (-a[i] + 1)
+    		int j = lower_bound(a, a + n, -a[i] + 1) - a;
+    		ans += (long long )(i - j); 
+    	} 
+    	return ans; 
+    }   
+};
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int array[n];
+		for (int i = 0; i < n; ++i)
+			cin>>array[i];
+	    Solution obj;
+		cout << obj.ValidPair(array, n) <<"\n";
+	}
+	return 0; 
+}
