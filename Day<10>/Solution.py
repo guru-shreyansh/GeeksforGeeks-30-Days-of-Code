2c82331c21d604f24df341719e308c2688e9f892
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-02-27 17:51:56 +0530

    Python

---
+++ Day<10>/Solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def repeatedStringMatch(self, A,B):
+        S=A
+        ret=1
+        
+        while len(S)<len(B):
+            S+=A
+            ret+=1
+        
+        if B in S:
+            return ret
+        
+        if B in S+A:
+            return ret+1
+        
+        return -1
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(t):
+        A=input().strip()
+        B=input().strip()
+        obj = Solution()
+        print(obj.repeatedStringMatch(A,B))
