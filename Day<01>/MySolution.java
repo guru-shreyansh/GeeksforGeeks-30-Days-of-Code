1aa26265ea477b8b471419143266389149599987
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-02-27 06:23:18 +0530

    My Java Solution

--- Day<01>/MySolution.java
+++ Day<01>/MySolution.java
@@ -1,1 +1,40 @@
+import java.io.*;
+import java.util.*;
+class GURU
+{
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(read.readLine());
+            
+            String input_line[] = read.readLine().trim().split("\\s+");
+            long a[] = new long[n];
+            for(int i=0; i<n; i++)
+                a[i] = Long.parseLong(input_line[i]);
 
+            Solution ob = new Solution();
+            ob.prank(a, n);
+
+            for (int i=0; i<n; i++)
+                System.out.print(a[i]+" ");
+            System.out.println();
+        }
+    } 
+} // Driver Code Ends
+
+class Solution
+{
+    void prank(long[] arr, int n)
+    {
+        long temp = 0;
+        long[] out = new long[n];
+        for (int i=0; i<n; i++)
+            out[i] = arr[ (int)arr[i] ];
+
+        System.arraycopy(out, 0, arr, 0, n);
+    }
+}
