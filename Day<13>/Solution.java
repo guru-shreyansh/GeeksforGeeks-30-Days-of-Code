84e92071ec5f39c2fde8f0fb0aa8062ff7491205
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-02-27 18:39:52 +0530

    Java

---
+++ Day<13>/Solution.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+        	int[] preorder = new int[n];
+        	int[] inorder = new int[n];
+        	int[] postorder = new int[n];
+            for(int i=0; i<n; i++)
+                preorder[i] = sc.nextInt();
+            for(int i=0; i<n; i++)
+                inorder[i] = sc.nextInt();
+            for(int i=0; i<n; i++)
+                postorder[i] = sc.nextInt();
+            Solution ob = new Solution();
+            if(ob.checktree(preorder, inorder, postorder, n) )
+			System.out.println("Yes");
+			else
+			System.out.println("No");
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+class Solution
+{
+    static boolean check(int preorder[], int i1, int inorder[], int i2, int postorder[], int i3, int len)
+    {
+        if(len==0) return true;
+        
+        // if array lengths are 1, then check if all of them are equal 
+        if(len==1) return ( (preorder[i1]==inorder[i2]) && (inorder[i2] == postorder[i3]) );
+        
+        // search for first element of preorder in inorder array 
+        int idx=-1;
+        for(int i=0 ; i<len ; i+=1)
+            if( inorder[i2+i] == preorder[i1] )
+            {
+                idx = i2+i;
+                break;
+            }
+        if(idx==-1) return false;
+        
+        // matching element in postorder array
+        if( preorder[i1] != postorder[i3+len-1] ) return false;
+        
+        // nodes in left subtree =
+        int left = idx-i2;
+        
+        // check for the left subtree 
+        boolean ret1 = check(preorder, i1+1, inorder, i2, postorder, i3, left);
+        
+        // check for the right subtree 
+        boolean ret2 = check(preorder , i1+1+left , inorder , i2+1+left , postorder , i3+left , len-left-1);
+        
+        // return 1 only if both of them are correct else 0 
+        return (ret1 && ret2);
+    }
+    static boolean checktree(int preorder[], int inorder[], int postorder[], int N)
+    {
+        return check(preorder, 0, inorder, 0, postorder, 0, N);
+    }
+}
