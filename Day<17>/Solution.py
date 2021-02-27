45d03f69793d2aab2d39b3d49669da79b7463c4e
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-02-27 19:53:45 +0530

    Python

---
+++ Day<17>/Solution.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+class Solution:
+    def help_classmate(self, arr, n):
+        stack = deque()
+        ret = [-1 for _ in range(n)]
+        
+        for i in range(n):
+            while len(stack) and arr[i] < arr[stack[-1]] :
+                ret[ stack.pop() ] = arr[i]
+            stack.append(i)
+        
+        return ret
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=="__main__":
+    for _ in range(int(input())):
+        n = int(input())
+        arr = [ int(x) for x in input().split() ]
+        obj = Solution()
+        result = obj.help_classmate(arr,n)
+        for i in result:
+            print(i,end=' ')
+        print()
