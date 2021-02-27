2b1bfddebc3e16cc41f7b79d1b87aa622696764e
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-02-27 22:39:28 +0530

    Java

---
+++ Day<18>/Solution.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG
+{
+    public static void main (String[] args)
+	{
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0)
+        {
+            int n = sc.nextInt();
+            int arr[]= new int[n];
+            for(int i=0; i<n; i++)
+                arr[i] = sc.nextInt();
+            Solution ob = new Solution();
+            if(ob.recreationalSpot(arr,n))
+                System.out.println("True");
+            else
+                System.out.println("False");
+        }
+	}
+}// } Driver Code Ends
+class Solution
+{
+    static boolean recreationalSpot(int[] arr , int n)
+    {
+        if (n < 3)
+            return false;
+        Stack < Integer > stack = new Stack < > ();
+        int[] temp = new int[n];
+        temp[0] = arr[0];
+        for(int i = 1; i <n; i++)
+            temp[i] = Math.min(temp[i - 1], arr[i]);
+        
+        for (int j = n - 1; j >= 0; j--)
+        {
+            if (arr[j] > temp[j]) 
+            {
+                while (!stack.empty() && stack.peek() <= temp[j])
+                    stack.pop();
+                if (!stack.empty() && stack.peek() < arr[j])
+                    return true;
+                stack.push(arr[j]);
+            }
+        }
+        return false;
+    }
+}
