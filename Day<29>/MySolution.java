00f31fb9cf46cb1d3cc2551d015ce6d9aa26c4da
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-02-28 11:31:32 +0530

    My Java Solution

---
+++ Day<29>/MySolution.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String str = sc.next();
+                 
+                    Sol obj = new Sol();
+                    System.out.println(obj.maxFrequency(str));
+                }
+        }
+}// } Driver Code Ends
+
+class Sol
+{
+    int maxFrequency(String S)
+    {
+        String prefix = "", suffix = "";
+        int N = S.length();
+        int start = 0, end = N-1;
+        int maxFreq = 1;
+        int freq = 0;
+        
+        while ( start <= end )
+        {
+            prefix = S.substring(0 , start+1);  // prefix + C[start];
+            suffix = S.substring(end);          // C[end] + suffix;
+            if ( prefix.equals(suffix) )
+            {
+                freq = ( N - S.replaceAll(prefix,"").length() ) / prefix.length();
+                if ( freq < maxFreq )           // If new Frequency is < Max Frequency
+                    return maxFreq;                 // found so far we can't get
+                maxFreq = Math.max( freq , maxFreq );   // a higher count
+            }
+            --end;
+            ++start;
+        }
+        return maxFreq;
+    }
+}
