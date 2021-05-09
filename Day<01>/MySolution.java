7ce1e3332434078e69500c442fffd537a92df4c7
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-05-09 21:18:48 +0530

    Update MySolution.java

--- Day<01>/MySolution.java
+++ Day<01>/MySolution.java
@@ -7,13 +7,12 @@ class GURU
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
         
-        while(t-- > 0)
+        while (t-- > 0)
         {
             int n = Integer.parseInt(read.readLine());
-            
             String input_line[] = read.readLine().trim().split("\\s+");
             long a[] = new long[n];
-            for(int i=0; i<n; i++)
+            for (int i=0; i<n; i++)
                 a[i] = Long.parseLong(input_line[i]);
 
             Solution ob = new Solution();
