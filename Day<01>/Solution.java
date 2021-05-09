9ab0d2a4b42f416948c8e6c61a909c1646b115cf
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-05-09 21:20:13 +0530

    Update Solution.java

--- Day<01>/Solution.java
+++ Day<01>/Solution.java
@@ -7,13 +7,12 @@ class GFG
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
         
-        while(t-- > 0)
+        while (t-- > 0)
         {
             int n = Integer.parseInt(read.readLine());
-            
             String input_line[] = read.readLine().trim().split("\\s+");
             long a[] = new long[n];
-            for(int i=0; i<n; i++)
+            for (int i=0; i<n; i++)
                 a[i] = Long.parseLong(input_line[i]);
 
             Solution ob = new Solution();
@@ -31,10 +30,10 @@ class Solution
     void prank(long[] a, int n)
     {
         long N = n;
-        for(int i = 0; i<n; i++)
-            a[i] = a[i] + ( a[ (int)a[i] ]%N ) * N;
+        for (int i = 0; i<n; i++)
+            a[i] = a[i] + ( a[ (int)a[i] ] % N ) * N;
 
-        for(int i=0; i<n; i++)
-            a[i] = a[i]/N;
+        for (int i=0; i<n; i++)
+            a[i] = a[i] / N;
     }
 }
