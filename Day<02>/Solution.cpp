c2573e1518ac791b74b414cde03c25566862288c
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-05-10 01:11:10 +0530

    C++

--- Day<02>/Solution.cpp
+++ Day<02>/Solution.cpp
@@ -10,10 +10,10 @@ class Solution
         long long base10num = N;
         long long base9num = 0;
         long long pos = 1;
-        while ( base10num>0 )
+        while ( base10num > 0 )
         {
             // taking remainder with 9
-            base9num += pos * (base10num%9);
+            base9num += pos * (base10num % 9);
             // dividing number with 9
             base10num /= 9;
             // multiplying position with 10
