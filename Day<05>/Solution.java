12cf07471b0bb00e983972657dbc874643c79958
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-05-13 19:38:04 +0530

    Java

--- Day<05>/Solution.java
+++ Day<05>/Solution.java
@@ -2,28 +2,28 @@ import java.util.*;
 
 class GFG
 {
-	public static void main(String args[])
-	{
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		while (t > 0)
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0)
+        {
+	    int n = sc.nextInt();
+	    int m = sc.nextInt();
+	    int k = sc.nextInt();
+	    int arr[][] = new int[n][m];
+	    for (int i=0; i<n; i++)
+	    {
+		for (int j=0; j<m; j++)
 		{
-			int n = sc.nextInt();
-			int m = sc.nextInt();
-			int k = sc.nextInt();
-			int arr[][] = new int[n][m];
-			for (int i=0; i<n; i++)
-			{
-				for (int j=0; j<m; j++ )
-				{
-					arr[i][j] = sc.nextInt();
-				}
-			}
-			Solution obj = new Solution();
-			System.out.println(obj.findK(arr, n, m, k));
-      t--;
+		    arr[i][j] = sc.nextInt();
 		}
+	    }
+	    Solution obj = new Solution();
+	    System.out.println(obj.findK(arr, n, m, k));
+	    --t;
 	}
+    }
 }
 
 class Solution
