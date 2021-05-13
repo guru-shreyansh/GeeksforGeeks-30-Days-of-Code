ac80bd08fcaa5385fab6491375b066e0c1538c1a
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-05-13 19:27:45 +0530

    My Java Solution

--- Day<05>/MySolution.java
+++ Day<05>/MySolution.java
@@ -2,65 +2,63 @@ import java.util.*;
 
 class GURU
 {
-	public static void main(String args[])
-	{
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		while (t > 0)
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0)
+        {
+	    int n = sc.nextInt();
+	    int m = sc.nextInt();
+	    int k = sc.nextInt();
+	    int arr[][] = new int[n][m];
+	    for (int i=0; i<n; i++)
+	    {
+		for (int j=0; j<m; j++)
 		{
-			int n = sc.nextInt();
-			int m = sc.nextInt();
-			int k = sc.nextInt();
-			int arr[][] = new int[n][m];
-			for (int i=0; i<n; i++)
-			{
-				for (int j=0; j<m; j++)
-				{
-					arr[i][j] = sc.nextInt();
-				}
-			}
-			Solution obj = new Solution();
-			System.out.println(obj.findK(arr, n, m, k));
-		    --t;
+		    arr[i][j] = sc.nextInt();
 		}
+	    }
+	    Solution obj = new Solution();
+	    System.out.println(obj.findK(arr, n, m, k));
+	    --t;
 	}
+    }
 }
 
 class Solution
 {
     static int findK(int arr[][], int rowEnd, int columnEnd, int K)
+    {
+	int k = 1;
+	int i, rowStart = 0, columnStart = 0;
+	while (rowStart < rowEnd && columnStart < columnEnd)
 	{
-	    int k = 1;
-	    int i, rowStart = 0, columnStart = 0;
+	    for (i = columnStart; i < columnEnd; ++i, ++k)
+		if ( k==K )
+		    return arr[rowStart][i];
+	    rowStart++;
 
-		while (rowStart < rowEnd && columnStart < columnEnd)
-		{
-			for (i = columnStart; i < columnEnd; ++i, ++k)
-			    if ( k==K )
-			        return arr[rowStart][i];
-			rowStart++;
+	    for (i = rowStart; i < rowEnd; ++i, ++k)
+		if ( k==K )
+		    return arr[i][columnEnd - 1];
+	    columnEnd--;
 
-			for (i = rowStart; i < rowEnd; ++i, ++k)
-			    if ( k==K )
-			        return arr[i][columnEnd - 1];
-			columnEnd--;
-
-			if (rowStart < rowEnd)
-			{
-				for (i = columnEnd - 1; i >= columnStart; --i, ++k)
-				    if ( k==K )
-				        return arr[rowEnd - 1][i];
-				rowEnd--;
-			}
-			
-			if (columnStart < columnEnd)
-			{
-				for (i = rowEnd - 1; i >= rowStart; --i, ++k)
-				    if ( k==K )
-				        return arr[i][columnStart];
-				columnStart++;
-			}
-		}
-		return -1;
+	    if (rowStart < rowEnd)
+	    {
+		for (i = columnEnd - 1; i >= columnStart; --i, ++k)
+		    if ( k==K )
+			return arr[rowEnd - 1][i];
+		rowEnd--;
+	    }
+	    if (columnStart < columnEnd)
+	    {
+		for (i = rowEnd - 1; i >= rowStart; --i, ++k)
+		    if ( k==K )
+			return arr[i][columnStart];
+		columnStart++;
+	    }
 	}
+	return -1;
+    }
 }
