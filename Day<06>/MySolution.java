10bd905ea7383b04f813a9a6eac634867351dabc
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-05-15 11:43:54 +0530

    My Java Solution

--- Day<06>/MySolution.java
+++ Day<06>/MySolution.java
@@ -7,14 +7,14 @@ class GURU
     {
 	Scanner sc = new Scanner(System.in);
 	int t = Integer.parseInt(sc.next());
-	while(t-- > 0)
+	while (t-- > 0)
 	{
 	    int n = Integer.parseInt(sc.next());
 	    int a[][] = new int[n][n];
 		    
-	    for(int i=0;i<n;i++)
+	    for (int i=0; i<n; i++)
 	    {
-		for(int j=0;j<n;j++)
+		for (int j=0; j<n; j++)
 		{
 		    a[i][j] = Integer.parseInt(sc.next());
 		}
