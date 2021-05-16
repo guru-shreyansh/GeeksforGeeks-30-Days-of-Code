64fa5b5aeadacfa6458c01161b77f101b36cdfe5
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-05-16 06:58:52 +0530

    Java

--- Day<07>/Solution.java
+++ Day<07>/Solution.java
@@ -1,34 +1,37 @@
 import java.io.*;
 import java.util.*; 
 
-class GFG{
-    public static void main(String args[]) throws IOException { 
+class GFG
+{
+    public static void main(String args[]) throws IOException
+    { 
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while(t > 0){
-        	int n = sc.nextInt();
-        	int array[] = new int[n];
-        	for (int i=0; i<n ;i++ ) {
-        		array[i] = sc.nextInt();
-        	}
+        while (t > 0)
+	{
+            int n = sc.nextInt();
+            int array[] = new int[n];
+            for (int i=0; i<n; i++)
+	    {
+		array[i] = sc.nextInt();
+	    }
             Solution ob = new Solution();
             System.out.println(ob.ValidPair(array,n));
             t--;
         }
-    } 
-} 
-// } Driver Code Ends
+    }
+}
 
-class Solution 
-{ 
+class Solution
+{
     static int lowerBound(int a[], int x)
     {
         int l = -1, r = a.length;
-        while(l + 1 < r)
+        while (l + 1 < r)
         {
             int m = (l+r)>>>1;
             
-            if(a[m] >= x) 
+            if (a[m] >= x)
                 r = m;
             else 
                 l = m;
@@ -37,16 +40,17 @@ class Solution
     }
 
     static long ValidPair(int a[], int n) 
-	{ 
-	    Arrays.sort(a);
-    	long ans = 0; 
-    	for (int i = 0; i < n; ++i) { 
-    		if (a[i] <= 0) 
-    			continue; 
-    		// search for first element >= (-a[i] + 1)
-    		int j = lowerBound(a, -a[i] + 1);
-    		ans += (i - j); 
-    	} 
-    	return ans; 
-	}
+    {
+	Arrays.sort(a);
+    	long ans = 0;
+    	for (int i = 0; i < n; ++i)
+	{
+    	    if (a[i] <= 0)
+    		continue;
+    	    // search for first element >= (-a[i] + 1)
+    	    int j = lowerBound(a, -a[i] + 1);
+    	    ans += (i - j);
+    	}
+    	return ans;
+    }
 }
