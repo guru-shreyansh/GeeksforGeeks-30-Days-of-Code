e04f7315ca4c2e25dfb67e44273276d54067deb6
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-05-19 07:46:09 +0530

    C++

--- Day<08>/Solution.cpp
+++ Day<08>/Solution.cpp
@@ -1,71 +1,58 @@
 #include<bits/stdc++.h> 
 using namespace std; 
 
- // } Driver Code Ends
-
-class Solution{
-    
+class Solution
+{
     public:
-    int maxCandy(int height[], int n) 
-    { 
-    
-    	// To store the maximum water so far 
-    	int maximum = 0; 
-    
-    	// Both the pointers are pointing at the first 
-    	// and the last buildings respectively 
-    	int i = 0, j = n - 1; 
-    
-    	// While the water can be stored between 
-    	// the currently chosen buildings 
-    	while (i < j) 
-    	{ 
-    
-    		// Update maximum water so far and increment i 
-    		if (height[i] < height[j]) 
-    		{ 
-    			maximum = max(maximum, (j - i - 1) * height[i]); 
-    			i++; 
-    		} 
-    
-    		// Update maximum water so far and decrement j 
-    		else if (height[j] < height[i]) 
-    		{ 
-    			maximum = max(maximum, (j - i - 1) * height[j]); 
-    			j--; 
-    		} 
-    
-    		// Any of the pointers can be updated (or both) 
-    		else
-    		{ 
-    			maximum = max(maximum, (j - i - 1) * height[i]); 
-    			i++; 
-    			j--; 
-    		} 
-    	} 
-    
-    	return maximum; 
-    }   
+    int maxCandy(int height[], int n)
+    {
+        // To store the maximum water so far
+        int maximum = 0;
+        // Both the pointers are pointing at the first and the last buildings respectively
+        int i = 0, j = n - 1;
+
+        // While the water can be stored between the currently chosen buildings
+        while (i < j)
+        {
+            // Update maximum water so far and increment i
+            if (height[i] < height[j])
+            {
+                maximum = max(maximum, (j - i - 1) * height[i]);
+                i++;
+            }
+            // Update maximum water so far and decrement j
+            else if (height[j] < height[i])
+            {
+                maximum = max(maximum, (j - i - 1) * height[j]);
+                j--;
+            }
+            // Any of the pointers can be updated (or both)
+            else
+            {
+                maximum = max(maximum, (j - i - 1) * height[i]);
+                i++;
+                j--;
+            }
+        }
+        return maximum;
+    }
 };
 
-// { Driver Code Starts.
-
-int main(){
+int main()
+{
     int t;
     cin>>t;
-    while(t--)
+    while (t--)
     {
-    	int n;
-    	cin>>n;
-    	int height[n];
-    	for (int i = 0; i < n; ++i)
-    	{
-    		cin>>height[i];
-    	}
-    	
-    	Solution obj;
-    	
-    	cout << obj.maxCandy(height,n)<<"\n";
+        int n;
+        cin>>n;
+        int height[n];
+        for (int i = 0; i < n; ++i)
+        {
+            cin>>height[i];
+        }
+        Solution obj;
+        cout << obj.maxCandy(height,n)<<"\n";
     }
     return 0;
 }
