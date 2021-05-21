637632a12a733c8b6f791bc9bb221f2ec697dc05
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-05-21 11:06:21 +0530

    My Java Solution

--- Day<10>/MySolution.java
+++ Day<10>/MySolution.java
@@ -1,44 +1,44 @@
 import java.io.*;
-import java.util.*; 
+import java.util.*;
 
-class GFG{
-    public static void main(String args[]) throws IOException { 
+class GURU
+{
+    public static void main(String args[]) throws IOException
+    { 
         Scanner sc = new Scanner(System.in);
         int t = Integer.parseInt(sc.nextLine());
-        while(t > 0){
+        while (t > 0)
+	{
             String A = sc.nextLine();
             String B = sc.nextLine();
             Solution ob = new Solution();
             System.out.println(ob.repeatedStringMatch(A,B));
             t--;
         }
+    }
+}
+
+class Solution
+{
+    static int repeatedStringMatch(String A, String B)
+    {
+        if ( A.indexOf(B) != -1 )
+            return 1;
+	    
+        StringBuilder aaa = new StringBuilder("");
+        StringBuilder bbb = new StringBuilder("");
+	    
+        int length = B.length()+A.length();
+        int repeated = 0;
+	    
+        while ( (aaa.indexOf(B) == -1) && (aaa.length() < length) )
+        {
+            aaa.append(A);
+            ++repeated;
+        }
+	    
+        if ( aaa.indexOf(B) == -1 )
+            return -1;
+        return repeated;
     } 
-} 
-        
-
-// } Driver Code Ends
-
-class Solution 
-{ 
-	static int repeatedStringMatch(String A, String B) 
-	{
-	    if ( A.indexOf(B) != -1 )
-	        return 1;
-	    
-	    StringBuilder aaa = new StringBuilder("");
-	    StringBuilder bbb = new StringBuilder("");
-	    
-	    int length = B.length()+A.length();
-	    int repeated = 0;
-	    
-	    while ( (aaa.indexOf(B) == -1) && (aaa.length() < length) )
-	    {
-	        aaa.append(A);
-	        ++repeated;	     
-	    }
-	    
-	    if ( aaa.indexOf(B) == -1 )
-	            return -1;
-	    return repeated;
-	} 
 }
