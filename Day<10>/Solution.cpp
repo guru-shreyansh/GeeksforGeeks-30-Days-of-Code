c539f4faa36ff9d13320d657ddfae84e83d978d3
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-05-21 11:11:13 +0530

    C++

--- Day<10>/Solution.cpp
+++ Day<10>/Solution.cpp
@@ -1,56 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
- // } Driver Code Ends
-class Solution{
-
+class Solution
+{
     private:
-    bool isSubString(string s, string sub) {
+    bool isSubString(string s, string sub)
+    {
         int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == sub[count]){
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] == sub[count])
                 count++;
-            } else {
+            else
                 i-=count;
                 count = 0;
-            }
-            if (count == sub.length()) {
+            if (count == sub.length())
                 return true;
-            }
         }
         return false;
     }
 
     public:
-    int repeatedStringMatch(string A, string B) {
+    int repeatedStringMatch(string A, string B)
+    {
         string temp = A;
         int count = ceil(float(B.length()) / float(A.length()));
-    
-        for(int i=1; i<count; i++)
+
+        for (int i=1; i<count; i++)
             A += temp;
-        
-        if(isSubString(A,B))
+
+        if (isSubString(A,B))
             return count;
         else
         {
             A += A;
-            if(isSubString(A,B))
+            if (isSubString(A,B))
                 return count+1;
         }
         return -1;
     }
-  
 };
 
-// { Driver Code Starts.
-
-
-int main(){
+int main()
+{
     int t;
     cin>>t;
-    while(t--)
+    while (t--)
     {
         string A,B;
         cin>>A;
