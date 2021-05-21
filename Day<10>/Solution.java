756367c49ff08f0cf26bf69617b317974592c11b
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-05-21 11:08:50 +0530

    Java

--- Day<10>/Solution.java
+++ Day<10>/Solution.java
@@ -1,53 +1,55 @@
 import java.io.*;
-import java.util.*; 
+import java.util.*;
 
-class GFG{
-    public static void main(String args[]) throws IOException { 
+class GfG
+{
+    public static void main(String args[]) throws IOException
+    { 
         Scanner sc = new Scanner(System.in);
         int t = Integer.parseInt(sc.nextLine());
-        while(t > 0){
+        while (t > 0)
+	{
             String A = sc.nextLine();
             String B = sc.nextLine();
             Solution ob = new Solution();
             System.out.println(ob.repeatedStringMatch(A,B));
             t--;
         }
-    } 
+    }
 }
 
 class Solution
-{ 
-	static boolean issubstring(String str2, String rep1) 
-	{ 
-		int M = str2.length(); 
-		int N = rep1.length(); 
+{
+    static boolean issubstring(String str2, String rep1)
+    {
+	int M = str2.length();
+	int N = rep1.length();
 
-		for (int i = 0; i <= N - M; i++) 
-		{ 
-			int j; 
-			for (j = 0; j < M; j++) 
-				if (rep1.charAt(i + j) != str2.charAt(j)) 
-					break; 
-			if (j == M) 
-				return true; 
-		}
-		return false; 
-	} 
+	for (int i = 0; i <= N - M; i++)
+	{
+	    int j;
+	    for (j = 0; j < M; j++)
+		if (rep1.charAt(i + j) != str2.charAt(j))
+		    break;
+	    if (j == M)
+		return true;
+	}
+	return false;
+    }
 
-	static int repeatedStringMatch(String A, String B) 
-	{ 
-		int ans = 1; 
-		String S = A; 
+    static int repeatedStringMatch(String A, String B)
+    {
+	int ans = 1;
+	String S = A; 
 		
-		while(S.length() < B.length()) 
-		{ 
-			S += A; 
-			ans++; 
-		} 
-		 
-		if (issubstring(B, S)) return ans; 
-		if (issubstring(B, S + A)) 
-			return ans + 1; 
-		return -1; 
+	while (S.length() < B.length()) 
+	{ 
+	    S += A; 
+	    ans++; 
 	} 
+	if (issubstring(B, S)) return ans; 
+	if (issubstring(B, S + A)) 
+	    return ans + 1; 
+	return -1; 
+    } 
 }
