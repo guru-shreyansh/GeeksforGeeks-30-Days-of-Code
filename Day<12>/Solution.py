6d1acfbb547fdcc8e37f212d615262f88a2cb43a
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-05-23 01:12:36 +0530

    Python

--- Day<12>/Solution.py
+++ Day<12>/Solution.py
@@ -1,23 +1,21 @@
 class Solution:
-    def sumBitDiff(self, arr, n): 
-        ans = 0  # Initialize result 
-      
-        # traverse over all bits 
-        for i in range(0, 32): 
-          
-            # count number of elements with i'th bit set 
+    def sumBitDiff(self, arr, n):
+        ans = 0  # Initialize result
+
+        # traverse over all bits
+        for i in range(0, 32):
+
+            # count number of elements with i'th bit set
             count = 0
-            for j in range(0,n): 
-                if ( (arr[j] & (1 << i)) ): 
+            for j in range(0,n):
+                if ( (arr[j] & (1 << i)) ):
                     count+=1
-      
-            # Add "count * (n - count) * 2" to the answer 
-            ans += (count * (n - count) * 2); 
-          
+
+            # Add "count * (n - count) * 2" to the answer
+            ans += (count * (n - count) * 2);
+
         return ans
-#{ 
-#  Driver Code Starts
-#Initial Template for Python 3
+
 
 if __name__=="__main__":
     t=int(input())
