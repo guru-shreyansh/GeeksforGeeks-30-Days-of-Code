6dadb71271768b2f7e36b940eef4cecb2d03070b
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-05-23 01:08:32 +0530

    Java

--- Day<12>/Solution.java
+++ Day<12>/Solution.java
@@ -3,43 +3,39 @@ import java.util.*;
 class GfG
 {
     public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-->0)
         {
-            Scanner sc = new Scanner(System.in);
-            int t = sc.nextInt();
-            while(t-->0)
-                {
-                    int n = sc.nextInt();
-                    int arr[] = new int[n];
-                    for(int i = 0 ;i<n;i++)
-                        arr[i] = sc.nextInt();
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0 ;i<n;i++)
+                arr[i] = sc.nextInt();
                     
-                    Solution obj = new Solution();
-                    System.out.println(obj.sumBitDiff(arr,n));
-                }
+            Solution obj = new Solution();
+            System.out.println(obj.sumBitDiff(arr,n));
         }
+    }
 }
 
-// } Driver Code Ends
 class Solution
 {
-    public static long sumBitDiff(int arr[], int n) 
-    { 
-        long ans = 0;  // Initialize result 
-      
-        // traverse over all bits 
-        for (int i = 0; i < 32; i++) 
-        { 
-            // count number of elements with i'th bit set 
-            int count = 0; 
-            for (int j = 0; j < n; j++) 
-                if ( (arr[j] & (1 << i)) != 0) 
-                    count++; 
-      
-            // Add "count * (n - count) * 2" to the answer 
-            ans += (count * (n - count) * 2); 
-        } 
-      
-        return ans; 
-    } 
-  
+    public static long sumBitDiff(int arr[], int n)
+    {
+        long ans = 0;  // Initialize result
+
+        // traverse over all bits
+        for (int i = 0; i < 32; i++)
+        {
+            // count number of elements with i'th bit set
+            int count = 0;
+            for (int j = 0; j < n; j++)
+                if ( (arr[j] & (1 << i)) != 0)
+                    count++;
+            // Add "count * (n - count) * 2" to the answer
+            ans += (count * (n - count) * 2);
+        }
+        return ans;
+    }
 }
