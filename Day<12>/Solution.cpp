6a1f065ba1ce5307e42c73a4f852bc64bf7ff9ce
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-05-23 01:11:33 +0530

    C++

--- Day<12>/Solution.cpp
+++ Day<12>/Solution.cpp
@@ -1,48 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
-//User function Template for C++
-
-class Solution{
+class Solution
+{
     public:
-    long long sumBitDiff(int arr[], int n) 
-    { 
-        long long ans = 0;  // Initialize result 
-      
-        // traverse over all bits 
-        for (int i = 0; i < 32; i++) 
-        { 
-            // count number of elements with i'th bit set 
-            int count = 0; 
-            for (int j = 0; j < n; j++) 
-                if ( (arr[j] & (1 << i)) ) 
-                    count++; 
-      
-            // Add "count * (n - count) * 2" to the answer 
-            ans += (count * (n - count) * 2); 
-        } 
-      
-        return ans; 
-    } 
-  
+    long long sumBitDiff(int arr[], int n)
+    {
+        long long ans = 0; // Initialize result
+        // traverse over all bits
+        for (int i = 0; i < 32; i++)
+        {
+            // count number of elements with i'th bit set
+            int count = 0;
+            for (int j = 0; j < n; j++)
+                if ( (arr[j] & (1 << i)) )
+                    count++;
+
+            // Add "count * (n - count) * 2" to the answer
+            ans += (count * (n - count) * 2);
+        }
+        return ans;
+    }
 };
 
-// { Driver Code Starts.
-  
 int main()
 {
-	int t;
+    int t;
     cin>>t;
-    while(t--)
+    while (t--)
     {
     	int n;
     	cin>>n;
     	int arr[n];
-    	for(int i = 0; i < n; i++)
-    		cin >> arr[i];
+    	for (int i = 0; i < n; i++)
+    	    cin >> arr[i];
     	Solution obj;
-		cout << obj.sumBitDiff(arr, n) << "\n";
+	cout << obj.sumBitDiff(arr, n) << "\n";
     }
-	return 0; 
+    return 0;
 }
