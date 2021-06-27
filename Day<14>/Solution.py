0e1df8385d54bc5845ccd665b3fca8d600e2cee9
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-06-27 21:36:17 +0530

    Python

--- Day<14>/Solution.py
+++ Day<14>/Solution.py
@@ -1,29 +1,24 @@
 class Solution:
-    
-    def digitSum(self, n): 
+
+    def digitSum(self, n):
     	sum = 0
-    	while (n): 
-    		sum += (n % 10) 
+    	while (n):
+    		sum += (n % 10)
     		n //= 10
     	return sum
 	
-    def RulingPair(self, arr, n): 
-    	mp = dict() 
+    def RulingPair(self, arr, n):
+    	map = dict()
     	ans = -1
-    	for i in range(n):  
-    		dSum = self.digitSum(arr[i]) 
-    
-    		if (dSum in mp): 
-    			ans = max(ans, mp[dSum] + arr[i]) 
-    		mp[dSum] = max(mp.get(dSum, 0) ,arr[i]) 
+    	for i in range(n):
+    		dSum = self.digitSum(arr[i])
+
+    		if (dSum in map):
+    			ans = max(ans, map[dSum] + arr[i])
+    		map[dSum] = max(map.get(dSum, 0) , arr[i])
     	return ans
 
-
-#{ 
-#  Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__': 
+if __name__ == '__main__':
     t = int(input())
     for _ in range(t):
         n = int(input())
