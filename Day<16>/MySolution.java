4ce7297b02784d10b3d42ed80cc7ac604927b41d
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-07-03 23:16:20 +0530

    My Java Solution

--- Day<16>/MySolution.java
+++ Day<16>/MySolution.java
@@ -1,23 +1,21 @@
 import java.util.*;
 import java.math.*;
 
-class Gfg
+class GURU
 {
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
         int t = Integer.parseInt(sc.next());
-        while(t-- > 0)
+        while (t-- > 0)
         {
             int k = Integer.parseInt(sc.next());
             String s = sc.next();
             Solution T = new Solution();
             System.out.println(T.reduced_String(k, s));
-            
         }
     }
 }
-// } Driver Code Ends
 
 class Solution
 {
@@ -26,19 +24,19 @@ class Solution
         if (k==1) return "";
         StringBuilder output = new StringBuilder(s);
         int count[] = new int[output.length()];
-    
-        for(int i=0; i<output.length(); i++)
+
+        for (int i=0; i<output.length(); i++)
         {
-            if( i==0 || output.charAt(i) != output.charAt(i-1) )
+            if ( i==0 || output.charAt(i) != output.charAt(i-1) )
                 count[i] = 1;
             else
             {
                 count[i] = count[i-1] + 1;
-                if(count[i]==k)
+                if (count[i]==k)
                 {
                     output.delete(i-k+1,i+1);
                     i = i-k;
-                }   
+                }
             }
         }
         return output.toString();
