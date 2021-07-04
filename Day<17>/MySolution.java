92edd1329a584b1b501aeef373e5fe8412475d0e
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-07-04 17:24:59 +0530

    My Java Solution

--- Day<17>/MySolution.java
+++ Day<17>/MySolution.java
@@ -1,36 +1,33 @@
-import java.util.*; 
-import java.io.*; 
-
-class GFG 
-{ 
-	public static void main (String[] args) { 
+import java.util.*;
+import java.io.*;
+class GURU
+{
+	public static void main (String[] args)
+	{
 		Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while(t > 0){
+        while (t > 0
+	    {
         	int n = sc.nextInt();
             int array[] = new int[n];
             for (int i = 0; i < n; ++i)
             {
                 array[i] = sc.nextInt();
             }
-
             Solution ob = new Solution();
-
             int ans[] = ob.help_classmate(array,n);
 
-           	for (int i=0; i<n; i++) 
-                System.out.print(ans[i]+" "); 
+           	for (int i=0; i<n; i++)
+                System.out.print(ans[i]+" ");
             System.out.println();
             t--;
         }
-	} 
-} 
-
-// } Driver Code Ends
+	}
+}
 
 class Solution
 {
-	public static int[] help_classmate(int arr[], int n) 
+	public static int[] help_classmate(int arr[], int n)
 	{
 	    int j=1;
 	    int[] help = new int[n];
@@ -45,5 +42,5 @@ class Solution
 	            help[i] = -1;
 	    }
 	    return help;
-	} 
+	}
 }
