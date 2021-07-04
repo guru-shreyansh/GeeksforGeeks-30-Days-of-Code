d7b3e6d70b0a0e35e15eb3861793d25dc61e2f14
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-07-04 17:28:01 +0530

    C++

--- Day<17>/Solution.cpp
+++ Day<17>/Solution.cpp
@@ -1,40 +1,37 @@
-#include <bits/stdc++.h> 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
-
- // } Driver Code Ends
-//User function Template for C++
-
-class Solution{
-    
+class Solution
+{
     public:
-    vector<int> help_classmate(vector<int> arr, int n) 
-    { 
-    	stack<int> s; 
+    vector<int> help_classmate(vector<int> arr, int n)
+    {
+    	stack<int> s;
     	vector<int> result(n,-1);
     	
-    	for (int i = 0; i < n; i++) { 
-    		if (s.empty()) { 
-    			s.push(i); 
-    			continue; 
-    		} 
-    		while (s.empty() == false && arr[s.top()] > arr[i]) { 
+    	for (int i = 0; i < n; i++)
+		{
+    		if (s.empty())
+			{
+    			s.push(i);
+    			continue;
+    		}
+    		while (s.empty() == false && arr[s.top()] > arr[i])
+			{
     			result[s.top()] = arr[i];
-    			s.pop(); 
-    		} 
-    		s.push(i); 
-    	} 
+    			s.pop();
+    		}
+    		s.push(i);
+    	}
     	return result;
-    } 
+    }
 };
 
-// { Driver Code Starts.
-
-int main() 
-{ 
+int main()
+{
 	int t;
 	cin>>t;
-	while(t--)
+	while (t--)
 	{
 		int n;
 		cin>>n;
@@ -51,5 +48,5 @@ int main()
 		}
 		cout<<"\n";
 	}
-	return 0; 
+	return 0;
 }
