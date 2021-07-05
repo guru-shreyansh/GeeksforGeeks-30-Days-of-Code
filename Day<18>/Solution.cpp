a893ee293ad322ea20099d6db9f18735bc1ec8e1
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-07-05 15:01:58 +0530

    C++

--- Day<18>/Solution.cpp
+++ Day<18>/Solution.cpp
@@ -1,24 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
-//User function template for C++
-
-class Solution{
-    
+class Solution
+{
     public:
-	bool recreationalSpot(int arr[], int n){
+	bool recreationalSpot(int arr[], int n)
+	{
 	    if (n < 3)
         	return false;
         stack<int> stack;
         int temp[n];
         temp[0] = arr[0];
-        for(int i = 1; i <n; i++)
+        for (int i = 1; i <n; i++)
             temp[i] = min(temp[i - 1], arr[i]);
-            
+        
         for (int j = n - 1; j >= 0; j--)
 		{
-            if (arr[j] > temp[j]) 
+            if (arr[j] > temp[j])
 			{
                 while (!stack.empty() && stack.top() <= temp[j])
                     stack.pop();
@@ -31,21 +29,19 @@ class Solution{
 	}
 };
 
-// { Driver Code Starts.
-
 int main()
 {
 	int t;
 	cin>>t;
-	while(t--)
+	while (t--)
 	{
 		int n;
 		cin>>n;
 		int arr[n];
-		for(int i=0;i<n;i++)
+		for (int i=0; i<n; i++)
 		    cin>>arr[i];
 		Solution ob;
-		if(ob.recreationalSpot(arr, n))
+		if (ob.recreationalSpot(arr, n))
 		    cout<<"True"<<endl;
 		else
 		    cout<<"False"<<endl;
