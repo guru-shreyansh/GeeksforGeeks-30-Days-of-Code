d1b618c3e6135075ca05d5655331e4333c2b82e0
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-07-07 12:26:24 +0530

    Java

--- Day<19>/Solution.java
+++ Day<19>/Solution.java
@@ -1,11 +1,13 @@
 import java.io.*;
 import java.util.*;
-class GFG {
-     public static void main(String args[]) throws IOException { 
+class GfG
+{
+     public static void main(String args[]) throws IOException
+     { 
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-
-        while(t > 0){
+        while (t > 0)
+        {
             int m = sc.nextInt();
             int n = sc.nextInt();
             Solution ob = new Solution();
@@ -13,37 +15,38 @@ class GFG {
             t--;
         }
     } 
-}// } Driver Code Ends
+}
 
-class Solution{
+class Solution
+{
     static int candies(int m, int n)
     {
         int X = (m * n) - m - n;
-
         HashSet<Integer> hash_set = new HashSet<>();
         Queue<Integer> queue = new LinkedList<>();
         queue.add(X);
         hash_set.add(X);
 
         int count = 0;
-        while (!queue.isEmpty()) {
-
+        while (!queue.isEmpty())
+        {
             int curr = queue.poll();
             count++;
             
             int key = curr-m;
-            if (key > 0 && hash_set.contains(key)==false ) {
+            if (key > 0 && hash_set.contains(key)==false )
+            {
                 queue.add(key);
                 hash_set.add(key);
             }
             
             key = curr - n;
-            if (key > 0 && hash_set.contains(key)==false) {
+            if (key > 0 && hash_set.contains(key)==false)
+            {
                 queue.add(key);
                 hash_set.add(key);
             }
         }
-
         return count;
     }
 }
