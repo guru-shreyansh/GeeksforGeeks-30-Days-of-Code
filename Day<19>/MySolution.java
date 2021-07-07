ed21c44923ce4e055e960ddd55ef52d17442ff31
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-07-07 12:25:04 +0530

    Update MySolution.java

--- Day<19>/MySolution.java
+++ Day<19>/MySolution.java
@@ -1,11 +1,13 @@
 import java.io.*;
 import java.util.*;
-class GFG {
-     public static void main(String args[]) throws IOException { 
+class GURU
+{
+     public static void main(String args[]) throws IOException
+     { 
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-
-        while(t > 0){
+        while (t > 0)
+        {
             int m = sc.nextInt();
             int n = sc.nextInt();
             Solution ob = new Solution();
@@ -13,12 +15,13 @@ class GFG {
             t--;
         }
     } 
-}// } Driver Code Ends
+}
 
 class Solution
 {
     static int candies(int m, int n)
-    {                               // Any number >= "LCM(m,n)-m-n" can be formed
+    {                               // Any number > "LCM(m,n)-m-n" can be formed
         return (m-1)*(n-1)/2;       // So we only need to check for numbers less
     }                               //      than or equal to (mn - m - n)
+    /** Chicken McNugget Problem / Two Stamp Problem / Frobenius Problem **/
 }
