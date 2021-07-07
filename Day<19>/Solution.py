89c7f2795d0baf8233e233ff7de7223985d0eebe
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-07-07 12:29:04 +0530

    Python

--- Day<19>/Solution.py
+++ Day<19>/Solution.py
@@ -1,37 +1,30 @@
 class Solution:
-    def candies(self, m, n): 
+    def candies(self, m, n):
     	i = 0
-    	X = (m * n) - m - n 
-    
-    	queue = [] 
-    	queue.append(X) 
-    	set = {X}
-    
-    	count = 0
-    	while (len(queue) > 0): 
-    		
-    		curr = queue[0] 
-    		queue.remove(queue[0]) 
-    
-    		count += 1
-    
-    		key = curr-m
-    		if (key > 0 and key not in set): 
-    			queue.append(key) 
-    			set.add(key)
-    
-    		key = curr-n
-    		if (key > 0 and key not in set): 
-    			queue.append(key) 
-    			set.add(key)
-    
-    	return count 
+    	X = (m * n) - m - n
 
-#{ 
-#  Driver Code Starts
-#Initial Template for Python 3
-	
-if __name__ == '__main__': 
+    	queue = []
+    	queue.append(X)
+    	set = {X}
+
+    	count = 0
+    	while (len(queue) > 0):
+    		curr = queue[0] 
+    		queue.remove(queue[0])
+
+    		count += 1
+    		key = curr-m
+    		if (key > 0 and key not in set):
+    			queue.append(key)
+    			set.add(key)
+
+    		key = curr-n
+    		if (key > 0 and key not in set):
+    			queue.append(key)
+    			set.add(key)
+    	return count
+
+if __name__ == '__main__':
     t = int(input())
     for _ in range(t):
     	arr = list(map(int, input().strip().split()))
