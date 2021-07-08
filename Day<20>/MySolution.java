64fe2199798f8966ee28c92574b3e9ca5e8acefe
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-07-08 23:40:08 +0530

    My Java Solution

--- Day<20>/MySolution.java
+++ Day<20>/MySolution.java
@@ -1,84 +1,70 @@
-import java.util.LinkedList; 
-import java.util.Queue; 
 import java.io.*;
 import java.util.*;
 
-class Node{
+class Node
+{
     int data;
     Node left;
     Node right;
-    Node(int data){
+    Node(int data)
+	{
         this.data = data;
         left=null;
         right=null;
     }
 }
 
-class GfG {
-    
-    static Node buildTree(String str){
-        
-        if(str.length()==0 || str.charAt(0)=='N'){
+class GURU
+{
+    static Node buildTree(String str)
+	{
+        if (str.length()==0 || str.charAt(0)=='N')
             return null;
-        }
         
         String ip[] = str.split(" ");
-        // Create the root of the tree
         Node root = new Node(Integer.parseInt(ip[0]));
-        // Push the root to the queue
         
         Queue<Node> queue = new LinkedList<>(); 
         
         queue.add(root);
-        // Starting from the second element
         
         int i = 1;
-        while(queue.size()>0 && i < ip.length) {
-            
-            // Get and remove the front of the queue
+        while(queue.size()>0 && i < ip.length)
+		{
             Node currNode = queue.peek();
             queue.remove();
-                
-            // Get the current node's value from the string
+            
             String currVal = ip[i];
-                
-            // If the left child is not null
-            if(!currVal.equals("N")) {
-                    
-                // Create the left child for the current node
+            
+            if(!currVal.equals("N"))
+			{
                 currNode.left = new Node(Integer.parseInt(currVal));
-                // Push it to the queue
                 queue.add(currNode.left);
             }
-                
-            // For the right child
             i++;
-            if(i >= ip.length)
+            if (ip.length <= i)
                 break;
-                
+
             currVal = ip[i];
-                
-            // If the right child is not null
-            if(!currVal.equals("N")) {
-                    
-                // Create the right child for the current node
+
+            if (!currVal.equals("N"))
+			{
                 currNode.right = new Node(Integer.parseInt(currVal));
-                    
-                // Push it to the queue
+                
                 queue.add(currNode.right);
             }
             i++;
         }
-        
         return root;
     }
     
-	public static void main (String[] args) throws IOException{
+	public static void main (String[] args) throws IOException
+	{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         int t=Integer.parseInt(br.readLine());
-        
-        while(t > 0){
+        while (t > 0)
+		{
             String line = br.readLine().trim();
             Node root = buildTree(line);
             
@@ -94,19 +80,6 @@ class GfG {
     }
 }
 
-// } Driver Code Ends
-/*
-class Node{
-    int data;
-    Node left;
-    Node right;
-    Node(int data){
-        this.data = data;
-        left=null;
-        right=null;
-    }
-}
-*/
 class Solution
 {
     static int range, sum, key;
@@ -120,9 +93,9 @@ class Solution
     }
     static int find(Node root, int k)
     {
-        if(root == null)
+        if (root == null)
             return -1;
-        if(root.data == key)
+        if (root.data == key)
         {
             sum += root.data;
             k--;
@@ -130,7 +103,7 @@ class Solution
             find(root.right, k);
             return k--;
         }
-        else if(k != range && k >= 0)
+        else if (k != range && 0 <= k)
         {
             sum += root.data;
             k--;
@@ -138,18 +111,19 @@ class Solution
             find(root.right, k);
             return -1;
         }
-        else if(k < 0)
+        else if (k < 0)
             return -1;
-        else{
+        else
+		{
             int k1 = find(root.left, k);
             int k2 = find(root.right, k);
-            if(k1 != -1)
+            if (k1 != -1)
             {
                 sum += root.data;
                 find(root.right, k1-1);
                 return k1-1;
             }
-            if(k2 != -1)
+            if (k2 != -1)
             {
                 sum += root.data;
                 find(root.left, k2-1);
