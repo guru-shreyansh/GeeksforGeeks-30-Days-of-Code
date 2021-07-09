6fb5d9aec237639adc4075e2256708a3822ace5d
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-07-09 11:13:20 +0530

    Python

--- Day<21>/Solution.py
+++ Day<21>/Solution.py
@@ -1,11 +1,11 @@
 '''
-# Node Class:
+#Node Class:
 class Node:
     def _init_(self,val):
         self.data = val
         self.left = None
         self.right = None
-        '''
+'''
 class Solution:
     def traverse(self, n):
         if not n:
@@ -25,43 +25,36 @@ class Solution:
             return vaccineKit+1
         return vaccineKit
 
-
-#{ 
-#  Driver Code Starts
-#Initial Template for Python 3
-
 from collections import deque
 
-# Tree Node
+#Tree Node
 class Node:
     def __init__(self, val):
         self.right = None
         self.data = val
         self.left = None
 
-
 # Function to Build Tree   
 def buildTree(s):
-    #Corner Case
-    if(len(s)==0 or s[0]=="N"):           
+    # Corner Case
+    if (len(s)==0 or s[0]=="N"):           
         return None
-       
-    # Creating list of strings from input 
-    # string after spliting by space
+    
+    # Creating list of strings from input string after spliting by space
     ip=list(map(str,s.split()))
-   
+    
     # Create the root of the tree
-    root=Node(int(ip[0]))                     
+    root=Node(int(ip[0]))
     size=0
     q=deque()
 
     # Push the root to the queue
-    q.append(root)                            
-    size=size+1 
+    q.append(root)
+    size=size+1
 
     # Starting from the second element
-    i=1                                       
-    while(size>0 and i<len(ip)):
+    i=1
+    while (0<size and i<len(ip)):
         # Get and remove the front of the queue
         currNode=q[0]
         q.popleft()
@@ -71,8 +64,7 @@ def buildTree(s):
         currVal=ip[i]
 
         # If the left child is not null
-        if(currVal!="N"):
-           
+        if (currVal!="N"):
             # Create the left child for the current node
             currNode.left=Node(int(currVal))
 
@@ -81,16 +73,14 @@ def buildTree(s):
             size=size+1
         # For the right child
         i=i+1
-        if(i>=len(ip)):
+        if (len(ip) <= i):
            break
         currVal=ip[i]
 
         # If the right child is not null
-        if(currVal!="N"):
-
+        if (currVal!="N"):
             # Create the right child for the current node
             currNode.right=Node(int(currVal))
-
             # Push it to the queue
             q.append(currNode.right)
             size=size+1
