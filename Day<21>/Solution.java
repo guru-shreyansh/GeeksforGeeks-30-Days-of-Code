4011808059cd71d55c90e51b6cd40576a7719e70
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-07-09 11:01:26 +0530

    Java

--- Day<21>/Solution.java
+++ Day<21>/Solution.java
@@ -1,26 +1,25 @@
-import java.util.LinkedList; 
-import java.util.Queue; 
 import java.io.*;
 import java.util.*;
 
-class Node{
+class Node
+{
     int data;
     Node left;
     Node right;
-    Node(int data){
+    Node(int data)
+    {
         this.data = data;
         left=null;
         right=null;
     }
 }
 
-class GFG {
-    
-    static Node buildTree(String str){
-        
-        if(str.length()==0 || str.charAt(0)=='N'){
+class GFG
+{
+    static Node buildTree(String str)
+    {
+        if (str.length()==0 || str.charAt(0)=='N')
             return null;
-        }
         
         String ip[] = str.split(" ");
         // Create the root of the tree
@@ -33,51 +32,50 @@ class GFG {
         // Starting from the second element
         
         int i = 1;
-        while(queue.size()>0 && i < ip.length) {
-            
+        while (0 < queue.size() && i < ip.length)
+        {
             // Get and remove the front of the queue
             Node currNode = queue.peek();
             queue.remove();
-                
+            
             // Get the current node's value from the string
             String currVal = ip[i];
-                
+            
             // If the left child is not null
-            if(!currVal.equals("N")) {
-                    
+            if (!currVal.equals("N"))
+            {
                 // Create the left child for the current node
                 currNode.left = new Node(Integer.parseInt(currVal));
                 // Push it to the queue
                 queue.add(currNode.left);
             }
-                
             // For the right child
             i++;
-            if(i >= ip.length)
+            if (ip.length <= i)
                 break;
-                
             currVal = ip[i];
-                
+            
             // If the right child is not null
-            if(!currVal.equals("N")) {
-                    
+            if (!currVal.equals("N"))
+            {
                 // Create the right child for the current node
                 currNode.right = new Node(Integer.parseInt(currVal));
-                    
+                
                 // Push it to the queue
                 queue.add(currNode.right);
             }
             i++;
         }
-        
         return root;
     }
     
-    public static void main (String[] args) throws IOException{
+    public static void main (String[] args) throws IOException
+    {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t=Integer.parseInt(br.readLine());
 
-        while(t > 0){
+        while (t > 0)
+        {
             String s = br.readLine();
             Node root = buildTree(s);
             
@@ -87,66 +85,51 @@ class GFG {
         }
     }
 }
-// } Driver Code Ends
- 
-/*  class Node
-{
-    int data;
-    Node left, right;
 
-    Node(int item)
-    {
-        data = item;
-        left = right = null;
-    }
-}   */
 class Solution
 {
     static int ans = 0;
-    
     public static int post(Node node)
     {
         boolean zero = false , two = false;
-        if(node.left==null && node.right==null)
+        if (node.left==null && node.right==null)
             return 0;
         
-        if(node.left!=null)
+        if (node.left!=null)
         {
             int get = post(node.left);
-            if(get==0)
+            if (get==0)
                 zero = true;
-            if(get==2)
+            if (get==2)
                 two = true;
-            
         }
         
-        if(node.right!=null)
+        if (node.right!=null)
         {
             int get = post(node.right);
-            if(get==0)
+            if (get==0)
                 zero = true;
-            if(get==2)
+            if (get==2)
                 two = true;
         }
         
-        if(zero==true)
+        if (zero==true)
         {
             ans++;
             return 2;
         }
         
-        if(two==true)
+        if (two==true)
         {
             return 1;
         }
         return 0; 
     }
-
     public static int supplyVaccine(Node root)
     {
-        ans = 0;    
+        ans = 0;
         int result = post(root);
-        if(result==0)
+        if (result==0)
             return ans + 1;
         return ans;
     }
