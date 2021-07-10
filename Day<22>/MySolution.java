20b7c04b33f625a1cbcdda6780850de92d842e6e
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-07-10 17:29:32 +0530

    My Java Solution

--- Day<22>/MySolution.java
+++ Day<22>/MySolution.java
@@ -1,92 +1,70 @@
-import java.util.LinkedList;
-import java.util.Queue;
 import java.io.*;
 import java.util.*;
 
-class Node {
+class Node
+{
     int data;
     Node left;
     Node right;
-    Node(int data) {
+    Node(int data)
+    {
         this.data = data;
         left = null;
         right = null;
     }
 }
 
-class GfG {
-
-    static Node buildTree(String str) {
-
-        if (str.length() == 0 || str.charAt(0) == 'N') {
+class GURU
+{
+    static Node buildTree(String str)
+    {
+        if (str.length() == 0 || str.charAt(0) == 'N')
             return null;
-        }
 
         String ip[] = str.split(" ");
-        // Create the root of the tree
         Node root = new Node(Integer.parseInt(ip[0]));
-        // Push the root to the queue
-
         Queue<Node> queue = new LinkedList<>();
 
         queue.add(root);
-        // Starting from the second element
-
         int i = 1;
-        while (queue.size() > 0 && i < ip.length) {
-
-            // Get and remove the front of the queue
+        while (0 < queue.size() && i < ip.length)
+		{
             Node currNode = queue.peek();
             queue.remove();
 
-            // Get the current node's value from the string
             String currVal = ip[i];
-
-            // If the left child is not null
-            if (!currVal.equals("N")) {
-
-                // Create the left child for the current node
+            if (!currVal.equals("N"))
+			{
                 currNode.left = new Node(Integer.parseInt(currVal));
-                // Push it to the queue
                 queue.add(currNode.left);
             }
-
-            // For the right child
             i++;
-            if (i >= ip.length) break;
-
+            if (ip.length <= i) break;
             currVal = ip[i];
 
-            // If the right child is not null
-            if (!currVal.equals("N")) {
-
-                // Create the right child for the current node
+            if (!currVal.equals("N"))
+			{
                 currNode.right = new Node(Integer.parseInt(currVal));
-
-                // Push it to the queue
                 queue.add(currNode.right);
             }
             i++;
         }
-
         return root;
     }
-    static void printInorder(Node root) {
+    static void printInorder(Node root)
+    {
         if (root == null) return;
-
         printInorder(root.left);
         System.out.print(root.data + " ");
-
         printInorder(root.right);
     }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
+	
+        while (t-- > 0)
+		{
             String s = br.readLine();
             Node root = buildTree(s);
             Solution sln = new Solution();
@@ -96,18 +74,20 @@ class GfG {
     }
 }
 
-class Range {
+class Range
+{
     int x, y;
-    Range() {
+    Range()
+	{
         this.x = 0;
         this.y = 0;
     }
-    Range(int x, int y) {
+    Range(int x, int y)
+	{
         this.x = x;
         this.y = y;
     }
 }
-// } Driver Code Ends
 class Solution
 {
     public static List<List<Integer>> levelOrderTraversal(Node root)
@@ -146,7 +126,7 @@ class Solution
 	        pq.add( new Triplet(result.get(i).get(0),i,0) );
 	        max = Math.max(max , result.get(i).get(0));
 	    }
-	    while( true )
+	    while ( true )
 	    {
 	        Triplet t = pq.poll();
 	        if ( (max-t.key+1)<range )
