2d3eb4779ee900a7775dbf7de3155bd6ee09b709
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-07-28 17:55:32 +0530

    Java

--- Day<24>/Solution.java
+++ Day<24>/Solution.java
@@ -1,36 +1,35 @@
 import java.io.*;
 import java.util.*;
+
 class GfG
 {
     public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-->0)
         {
-            Scanner sc = new Scanner(System.in);
-            int t = sc.nextInt();
-            while(t-->0)
-                {
-                    int n = sc.nextInt();
-                    ArrayList<Integer> arr = new ArrayList<Integer>();
-                    ArrayList<Integer> res = new ArrayList<Integer>();
-                    for(int i=0; i<n; i++)
-                        {
-                            int x = sc.nextInt();
-                            arr.add(x);
-                        }
-                    int k = sc.nextInt();
-                    
-                    Solution obj = new Solution();
-                    res = obj.TopK(arr,k);
-                    
-                    for(int i=0; i<res.size();i++)
-                        {
-                            System.out.print(res.get(i) + " ");
-                        }
-                    System.out.println();    
-                    
-                        
-                }
+            int n = sc.nextInt();
+            ArrayList<Integer> arr = new ArrayList<Integer>();
+            ArrayList<Integer> res = new ArrayList<Integer>();
+            for (int i=0; i<n; i++)
+            {
+                int x = sc.nextInt();
+                arr.add(x);
+            }
+            int k = sc.nextInt();
+            
+            Solution obj = new Solution();
+            res = obj.TopK(arr,k);
+            
+            for (int i=0; i<res.size(); i++)
+            {
+                System.out.print(res.get(i) + " ");
+            }
+            System.out.println();
         }
-}// } Driver Code Ends
+    }
+}
 
 class Solution
 {
@@ -38,29 +37,28 @@ class Solution
     {
         int i,j=0;
         int arr[] = new int[10002];
-        for(i=0;i<10002;i++)
+        for (i=0; i<10002; i++)
             arr[i] = 0;
             
-        for(i=0;i<array.size();i++)
+        for (i=0; i<array.size(); i++)
             arr[array.get(i)]++;
         
-        
         ArrayList<Integer>res[] = new ArrayList[100002];
-        for(i=0;i<100002;i++)
+        for (i=0; i<100002; i++)
         {
             ArrayList<Integer>temp = new ArrayList<Integer>();
             res[i]=temp;
         }
-        for(i=0;i<10002;i++)
-            if(arr[i]!=0)
+        for (i=0; i<10002; i++)
+            if (arr[i]!=0)
                 res[arr[i]].add(i);
- 
+
         ArrayList<Integer>ans = new ArrayList<Integer>();
-        for(i=100001;i>=0;i--)
+        for (i=100001; 0<=i; i--)
         {
-            for(int p = res[i].size()-1;p>=0;p--)
+            for (int p=res[i].size()-1; 0<=p; p--)
             {
-                if(j!=k)
+                if (j!=k)
                     ans.add(res[i].get(p));
                 else
                     break;
