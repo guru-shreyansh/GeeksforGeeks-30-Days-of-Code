ef1430de2d2f202bd2a5a1bdad3dc13d2e358b50
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-07-28 17:58:32 +0530

    C++

--- Day<24>/Solution.cpp
+++ Day<24>/Solution.cpp
@@ -1,70 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
- // } Driver Code Ends
-class Solution{
+class Solution
+{
     public:
     vector<int> TopK(vector<int>& array, int k)
     {
         int i,j=0;
         int arr[10002];
-        for(i=0;i<10002;i++)
+        for (i=0; i<10002; i++)
             arr[i] = 0;
-            
-        for(i=0;i<array.size();i++)
+        for (i=0; i<array.size(); i++)
             arr[array[i]]++;
         
         vector<int>res[100002];
         
-        for(i=0;i<10002;i++)
-            {
-                if(arr[i]!=0)
-                    res[arr[i]].push_back(i);
-            }
-            
+        for (i=0; i<10002; i++)
+        {
+            if (arr[i]!=0)
+                res[arr[i]].push_back(i);
+        }
+        
         vector<int>ans;
-        for(i=100001;i>=0;i--)
+        for (i=100001; 0<=i; i--)
+        {
+            for (int p=res[i].size()-1; 0<=p; p--)
             {
-                for(int p = res[i].size()-1;p>=0;p--)
-                    {
-                        if(j!=k)
-                            ans.push_back(res[i][p]);
-                        else
-                            break;
-                        j++;
-                    }
+                if (j!=k)
+                    ans.push_back(res[i][p]);
+                else
+                    break;
+                j++;
             }
+        }
         return ans;
     }
 };
-
-// { Driver Code Starts.
 
 int main()
 {
     int t;
     cin>>t;
-    while(t--)
+    while (t--)
     {
-        int n,k ;
+        int n,k;
         cin>>n;
         
         vector<int>array(n);
         
-        for(int i=0; i<n; i++)
+        for (int i=0; i<n; i++)
             cin>>array[i];
-            
         cin>>k;
         
         Solution obj;
         vector<int> result = obj.TopK(array,k);
         
-        for(int i=0; i<result.size();i++)
+        for (int i=0; i<result.size();i++)
             cout<<result[i]<<" ";
 
         cout<<"\n";
-
     }
     return 0;
 }
