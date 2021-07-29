11dd14450b96bf7631c5d1e3234751a279913a5e
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-07-29 13:14:34 +0530

    My Java Solution

--- Day<25>/MySolution.java
+++ Day<25>/MySolution.java
@@ -32,7 +32,7 @@ class GURU
             Solution T = new Solution();
             int ans[][] = T.findDistance(mat, m, n);
             
-            for(int i=0; i<m; i++)
+            for (int i=0; i<m; i++)
             {
                 for (int j=0; j<n; j++)
                 {
