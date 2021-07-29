78b5ada810e69e9f9c6f3c282023f05ba11dfdc4
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-07-29 13:22:54 +0530

    Python

--- Day<25>/Solution.py
+++ Day<25>/Solution.py
@@ -7,30 +7,27 @@ class Solution:
     def findDistance(self, matrix, m, n):
         ret = [ [-1 for _ in range(n)] for _ in range(m) ]
         q = deque()
-    
+        
         for i in range(m):
             for j in range(n):
                 if matrix[i][j] == 'B':
                     ret[i][j] = 0
                     q.append((i,j))
-    
+        
         while len(q):
             r=q[0][0]
             c=q[0][1]
             q.popleft()
-    
+            
             for i in range(4):
                 nextr=r+dr[i]
                 nextc=c+dc[i]
-                if nextr>=0 and nextr<m and nextc>=0 and nextc<n:
+                if 0<=nextr and nextr<m and 0<=nextc and nextc<n:
                     if ret[nextr][nextc]==-1 and matrix[nextr][nextc]=='O':
                         ret[nextr][nextc] = ret[r][c]+1
                         q.append((nextr,nextc))
         return ret
 
-
-#{ 
-#  Driver Code Starts
 
 if __name__=="__main__":
     t=int(input())
@@ -41,7 +38,7 @@ if __name__=="__main__":
         matrix=[]
         for _ in range(m):
             matrix.append( [ x for x in input().strip().split() ] )
-        obj = Solution() 
+        obj = Solution()
         result = obj.findDistance(matrix,m,n)
         for i in result:
             for j in i:
