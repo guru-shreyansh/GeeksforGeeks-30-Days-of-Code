ce28c931d1b5d538c7f044f3bf42106f149d7c5d
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-07-31 20:58:22 +0530

    C++

--- Day<26>/Solution.cpp
+++ Day<26>/Solution.cpp
@@ -2,23 +2,22 @@
 using namespace std;
 #define endl '\n'
 
-
- // } Driver Code Ends
-
-class Solution{
+class Solution
+{
     public:
-    int minTime(vector<pair<int, int>> &dependency, int duration[], int n, int m) {
+    int minTime(vector<pair<int, int>> &dependency, int duration[], int n, int m)
+    {
         vector<int> v[n+1];
         int vis[n+1]={0},deg[n+1]={0},req[n+1]={0},ans=0;
-        for(auto i:dependency)
+        for (auto i:dependency)
         {
             v[i.first].push_back(i.second);
             deg[i.second]++;
         }
         vector<int> q;
-        for(int i=0;i<n;i++)
+        for (int i=0; i<n; i++)
         {
-            if(deg[i]==0)
+            if (deg[i]==0)
             {
                 vis[i]=1;
                 q.push_back(i);
@@ -26,40 +25,42 @@ class Solution{
             req[i]=duration[i];
             ans=max(ans,req[i]);
         }
-        while(q.size())
+        while (q.size())
         {
             int node=q.back();
             q.pop_back();
-            for(auto i:v[node])
+            for (auto i:v[node])
             {
                 deg[i]--;
-                req[i]=max(req[node]+duration[i],req[i]);
-                if(deg[i]==0){
-                q.push_back(i);
-                vis[i]=1;
-                ans=max(ans,req[i]);
+                req[i] = max(req[node]+duration[i],req[i]);
+                if (deg[i]==0)
+                {
+                    q.push_back(i);
+                    vis[i] = 1;
+                    ans = max(ans,req[i]);
                 }
             }
         }
-        for(int i=0;i<n;i++)
-        if(vis[i]==0)
-        return -1;
+        for (int i=0; i<n; i++)
+            if (vis[i]==0)
+                return -1;
         return ans;
     }
 };
 
-// { Driver Code Starts.
-
-int main() {
+int main()
+{
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         int n, m, x, y;
         cin >> n >> m;
         int duration[n + 5];
         vector<pair<int, int>> dependency;
         for (int i = 0; i < n; i++) cin >> duration[i];
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < m; i++)
+        {
             cin >> x >> y;
             dependency.push_back({x, y});
         }
