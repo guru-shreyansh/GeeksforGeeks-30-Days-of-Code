da5701f5ce54a17050141c27d17cf3750332607c
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-08-01 12:59:54 +0530

    Python

--- Day<27>/Solution.py
+++ Day<27>/Solution.py
@@ -1,40 +1,36 @@
 class Solution:
-    def catchThieves(self, arr, n, k): 
+    def catchThieves(self, arr, n, k):
     	i = 0
     	l = 0
     	r = 0
     	result = 0
-    	thief = [] 
-    	police = [] 
-    
-    	while i < n: 
-    		if arr[i] == 'P': 
-    			police.append(i) 
-    		elif arr[i] == 'T': 
-    			thief.append(i) 
+    	thief = []
+    	police = []
+
+    	while i < n:
+    		if arr[i] == 'P':
+    			police.append(i)
+    		elif arr[i] == 'T':
+    			thief.append(i)
     		i += 1
-    
-    	# track lowest current indices of 
-    	# thief: thief[l], police: police[r] 
-    	while l < len(thief) and r < len(police): 
+
+    	# track lowest current indices of thief: thief[l], police: police[r]
+    	while l < len(thief) and r < len(police):
     		
-    		# can be caught 
-    		if (abs( thief[l] - police[r] ) <= k): 
+    		# can be caught
+    		if (abs( thief[l] - police[r] ) <= k):
     			result += 1
     			l += 1
     			r += 1
-    			
-    		# increment the minimum index 
-    		elif thief[l] < police[r]: 
+    		
+    		# increment the minimum index
+    		elif thief[l] < police[r]:
     			l += 1
-    		else: 
+    		else:
     			r += 1
-    
     	return result
-#{ 
-#Driver Code Starts.
 
-if __name__=='__main__': 
+if __name__=='__main__':
 	t = int(input())
 	for _ in range(t):
 		line1 = list(map(int, input().strip().split()))
