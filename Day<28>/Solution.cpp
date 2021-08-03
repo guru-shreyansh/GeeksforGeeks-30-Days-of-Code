9c0d954d1802b3ef9ded1ea5066ed19e19ca0717
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-08-03 15:20:26 +0530

    C++

--- Day<28>/Solution.cpp
+++ Day<28>/Solution.cpp
@@ -1,25 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
- // } Driver Code Ends
-//User function Template for C++
-
-class Solution{
+class Solution
+{
     public:
     int min_sprinklers(int gallery[], int n)
     {
         vector<pair<int,int>> sprinklers;
-        for(int i=0; i<n; i++)
-            if(gallery[i] > -1)
+        for (int i=0; i<n; i++)
+            if (-1 < gallery[i])
                 sprinklers.push_back( pair<int,int> ( i-gallery[i], i+gallery[i] ) );
         
         sort(sprinklers.begin(), sprinklers.end());
         
         int target=0, sprinklers_on=0, i=0;
-        while(target<n)
+        while (target < n)
         {
-            if(i==sprinklers.size() || sprinklers[i].first>target)
+            if (i==sprinklers.size() || target<sprinklers[i].first)
                 return -1;
             
             int max_range = sprinklers[i].second;
@@ -28,35 +25,30 @@ class Solution{
                 i++;
                 max_range = max( max_range,  sprinklers[i].second );
             }
-            if(max_range<target)
+            if (max_range < target)
                 return -1;
             
             sprinklers_on++;
-            target = max_range +1;
+            target = max_range + 1;
             i++;
         }
-        
         return sprinklers_on;
     }
 };
-
-
-// { Driver Code Starts.
 
 int main()
 {
     int t;
     cin>>t;
-    while(t--)
+    while (t--)
     {
         int n;
         cin>>n;
-        
         int gallery[n];
-        for(int i=0; i<n; i++)
+        for (int i=0; i<n; i++)
             cin>> gallery[i];
         Solution obj;
-        cout<< obj.min_sprinklers(gallery,n) << endl;
+        cout << obj.min_sprinklers(gallery,n) << endl;
     }
 	return 1;
 }
