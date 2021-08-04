7d1c2315f626c5a4dd9050afe37106fd073e15ff
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-08-04 21:32:24 +0530

    C++

--- Day<29>/Solution.cpp
+++ Day<29>/Solution.cpp
@@ -1,115 +1,85 @@
-import java.io.*;
-import java.util.*;
-class GfG
-{
-    public static void main(String args[])
-        {
-            Scanner sc = new Scanner(System.in);
-            int t = sc.nextInt();
-            while(t-->0)
-                {
-                    String str = sc.next();
-                 
-                    Sol obj = new Sol();
-                    System.out.println(obj.maxFrequency(str));
-                }
-        }
-}// } Driver Code Ends
+#include <bits/stdc++.h>
+using namespace std;
 
-class Sol
+int main() 
 {
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   	int t;
+   	cin >> t;
+   	while (t--)
+   	{
+   		string str;
+   		cin >> str;
+   		Solution obj;
+   		cout << obj.maxFrequency(str) << "\n";
+   	}
+    return 0;
+}
+
+class Solution
+{
+    public:
+    vector<int> z_function(string s)
+    {
+    	int n = s.size();
+    	vector<int> z(n);
+    	for (int i = 1, l = 0, r = 0; i < n; i++)
+    	{
+    		if (i <= r)
+    			z[i] = min(r - i + 1, z[i - l]);
+    		
+    		while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+    			z[i]++;
+    		
+    		if (r < i + z[i] - 1)
+    			l = i, r = i + z[i] - 1;
+    	}
+    	return z;
+    }
     
-ArrayList<Integer> z_function(String s)
-{
-	int n = s.length();
-	ArrayList<Integer> z = new ArrayList<Integer>(n);
-	
-	for(int i = 0 ;i<n;i++)
-	    z.add(i,0);
-
-	for(int i = 1, l = 0, r = 0; i < n; i++)
-	{
-
-		if(i <= r)
-			z.set(i , Math.min(r - i + 1, z.get(i - l)) );
-		
-		while(i + z.get(i) < n && s.charAt(z.get(i)) == s.charAt(i + z.get(i)) )
-			z.set(i , z.get(i) + 1);
-		
-		
-		if(i + z.get(i) - 1 > r)
-		{
-			l = i;
-			r = i + z.get(i) - 1;
+    void update(int idx, int val, vector<int> &bit, int n)
+    {
+    	if (idx == 0)
+    		return;
+    	while (idx <= n)
+    	{
+    		bit[idx] += val;
+    		idx += (idx&-idx);
+    	}
+    }
+    
+    int pref(int idx, vector<int> &bit)
+    {
+    	int ans = 0;
+    	while (0 < idx)
+    	{
+    		ans += bit[idx];
+    		idx -= (idx&-idx);
 		}
-	}
-	
-    return z;
-}
-        
- 
- 
-void update(int idx, int val, ArrayList<Integer> bit, int n)
-{
-	if(idx == 0)
-		return;
-
-	while(idx <= n)
-	{
-		bit.set(idx , bit.get(idx) + val);
-		idx += (idx&-idx);
-	}
-}
- 
- 
-int pref(int idx, ArrayList<Integer> bit)
-{
-	int ans = 0;
-	while(idx > 0)
-	{
-		ans += bit.get(idx); 
-		idx -= (idx&-idx);
-	}	
-
-	return ans;
-}
-
-
-int maxFrequency(String str)
-{
-	int n = str.length();
-	ArrayList<Integer> z = z_function(str);
-	
-
-	ArrayList<Integer> bit = new ArrayList<Integer>(n + 5);
-	for(int i = 0 ;i<n + 5;i++)
-    	    bit.add(i,0);
-	
-	for(int i = 1; i < n; i++)
-		update(z.get(i), 1, bit, n);
-
-
-    TreeMap<Integer,Integer> m = new TreeMap<Integer,Integer>();
-
-	for(int i = n-1; i >= 1; i--)
-	{
-		if(z.get(i) != (n-i))
-			continue;
+    	return ans;
+    }
+    
+    int maxFrequency(string str)
+    {
+    	int n = str.size();
+    	vector<int> z = z_function(str);
+    	vector<int> bit(n+5, 0);
 		
-		if(m.containsKey(z.get(i)))
-		    {
-		        m.put(z.get(i), m.get(z.get(i)) +   (pref(n, bit) - pref(z.get(i) - 1, bit) + 1)  ); 
-		    }
-		else
-		    m.put(z.get(i) , (pref(n, bit) - pref(z.get(i) - 1, bit) + 1) );
-	
-	}
-	
-	int ans = 1;
-	
-	for(Map.Entry<Integer,Integer> entry : m.entrySet())
-		ans = Math.max(ans, entry.getValue());
-
-	return ans;
-}
-}
+    	for (int i = 1; i < n; i++)
+    		update(z[i], 1, bit, n);
+		
+    	map<int, int> m;
+    	for (int i = n-1; 1 <= i; i--)
+    	{
+    		if (z[i] != (n-i))
+    			continue;
+    		m[z[i]] += (pref(n, bit) - pref(z[i]-1, bit) + 1);
+    	}
+    	int ans = 1;
+    	for (auto it:m)
+    		ans = max(ans, it.second);
+    	return ans;
+    }
+};
