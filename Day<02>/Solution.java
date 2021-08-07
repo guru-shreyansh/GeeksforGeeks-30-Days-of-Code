68061c95be82d398ef37a921eefd6a10f5532c60
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-08-07 22:11:01 +0530

    Java

--- Day<02>/Solution.java
+++ Day<02>/Solution.java
@@ -1,20 +1,20 @@
 import java.io.*;
 import java.util.*;
 
-class GFG
+class GfG
 {
     public static void main (String[] args)
     {
-	  Scanner ob = new Scanner(System.in);
-	  int t = ob.nextInt();
-	  while (t-->0)
-	  {
-		long n = ob.nextLong();
-		Solution ab = new Solution();
-		long k = ab.findNth(n);
-		System.out.println(k);
-	  }
-     }
+		Scanner ob = new Scanner(System.in);
+		int t = ob.nextInt();
+		while (t-->0)
+		{
+			long n = ob.nextLong();
+			Solution ab = new Solution();
+			long k = ab.findNth(n);
+			System.out.println(k);
+		}
+	}
 }
 
 class Solution
