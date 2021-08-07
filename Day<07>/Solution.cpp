5f67d9165ff56df2f9ba824915680840f14f36e8
Shreyansh Kumar Singh <49881504+guru-shreyansh@users.noreply.github.com>
2021-08-07 22:45:43 +0530

    C++

--- Day<07>/Solution.cpp
+++ Day<07>/Solution.cpp
@@ -9,9 +9,9 @@ class Solution
     	sort(a, a+n);
     	long long ans = 0;
     	for (int i = 0; i < n; ++i)
-	{
-	    if (a[i] <= 0)
-    		continue;
+		{
+	    	if (a[i] <= 0)
+    			continue;
     	    // search for first element >= (-a[i] + 1)
     	    int j = lower_bound(a, a + n, -a[i] + 1) - a;
     	    ans += (long long )(i - j);
@@ -26,13 +26,13 @@ int main()
     cin>>t;
     while (t--)
     {
-	int n;
-	cin>>n;
-	int array[n];
-	for (int i = 0; i < n; ++i)
-	    cin>>array[i];
-	 Solution obj;
-	 cout << obj.ValidPair(array, n) <<"\n";
+		int n;
+		cin>>n;
+		int array[n];
+		for (int i = 0; i < n; ++i)
+	    	cin>>array[i];
+	 	Solution obj;
+	 	cout << obj.ValidPair(array, n) <<"\n";
     }
     return 0; 
 }
